bc0198e347fa998cda6d77cd48a8f587bb81aa30
Teja Sri <iamtejasri01@gmail.com>
2026-02-10 17:41:23 +0530

    adding file

---
+++ variablees.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Suresh
+PERSON2=Ramesh
+echo "$PERSON1 :: Hello $PERSON2,How are you?"
+echo "$PERSON2 :: Hi $PERSON1,I am fine thanks for asking"
+echo "$PERSON1 :: doing fine,what are you learning"
+echo "$PERSON2 :: I am learning shell"
