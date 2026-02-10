8cdaa65d71b1f667288d30f492ebe2de74337b79
Teja Sri <iamtejasri01@gmail.com>
2026-02-10 17:46:24 +0530

    adding file

--- variablees.sh
+++ variablees.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=Suresh
-PERSON2=Ramesh
+PERSON1=Trump
+PERSON2=Putin
 echo "$PERSON1 :: Hello $PERSON2,How are you?"
 echo "$PERSON2 :: Hi $PERSON1,I am fine thanks for asking"
 echo "$PERSON1 :: doing fine,what are you learning"
