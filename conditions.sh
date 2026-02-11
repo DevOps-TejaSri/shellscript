e5caf8a9a9c54fc22cb5f0253c5baa0937d43997
Teja Sri <iamtejasri01@gmail.com>
2026-02-11 12:26:02 +0530

    adding text

--- conditions.sh
+++ conditions.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-NUMBER= 25
+
+NUMBER=25
+
 # -gt -> greater than
 # -lt -> less than
 # -eq -> equal
 # -ne -> not equal
-if [ $NUMBER -gt 20 ] ; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given number $NUMBER is greater than 20"
-elif [ $NUMBER -eq 20 ] ; then
+elif [ $NUMBER -eq 20 ]; then
      echo "Given number $NUMBER is equal to 20"
 else
      echo "Given number $NUMBER is less than 20"
