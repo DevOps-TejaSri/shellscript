fc0591700b06de7956809db80a5fa9e3173392a8
Teja Sri <iamtejasri01@gmail.com>
2026-02-11 18:25:03 +0530

    adding text

---
+++ loops1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/c/DevOps/shellscript "
+LOGS_FILE="/c/DevOps/shellscript/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please run this script with root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+     if [ $1 -ne 0 ]; then
+        echo "$2 ... failed" | tee -a $LOGS_FIL
+        exit 1
+     else 
+        echo "$2 .... Success" | tee -a $LOGS_FIL
+    fi
+}
+
+for package in $@
+do 
+    dnf install $package -y &>>$LOGS_FILE
+    VALIDATE $? "$packae installation"
+done
